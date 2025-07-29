7ed4d73c316224e5699b01e0da2e02722b792cab
david <github@nonswag.dev>
2025-07-29 22:10:52 +0200

    Annotate `PlayerData` interface with `@ApiStatus.NonExtendable`
    Added `@ApiStatus.NonExtendable` to prevent extension of `PlayerData`. Included `@since 0.1.0` documentation for version tracking.

--- api/src/main/java/net/thenextlvl/perworlds/data/PlayerData.java
+++ api/src/main/java/net/thenextlvl/perworlds/data/PlayerData.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.util.Vector;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Range;
 import org.jetbrains.annotations.Unmodifiable;
 import org.jspecify.annotations.NullMarked;
@@ -22,7 +23,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * @since 0.1.0
+ */
 @NullMarked
+@ApiStatus.NonExtendable
 public interface PlayerData {
     WorldGroup group();
 
