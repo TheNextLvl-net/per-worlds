e268048a00037ab610b3ebaa5460157d5b8c58e3
david <github@nonswag.dev>
2025-07-31 17:14:44 +0200

    Annotate `stats` method with `@ApiStatus.Experimental`
    Marked the `stats` method as experimental to indicate its provisional status and potential future changes.

--- api/src/main/java/net/thenextlvl/perworlds/data/PlayerData.java
+++ api/src/main/java/net/thenextlvl/perworlds/data/PlayerData.java
@@ -213,6 +213,7 @@ public interface PlayerData {
     @Contract(pure = true)
     Set<NamespacedKey> discoveredRecipes();
 
+    @ApiStatus.Experimental
     Stats stats();
 
     @Contract(pure = true)
