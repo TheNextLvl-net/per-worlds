95d30417c8e8eac5c2293ff58a04c562e3a0b423
david <github@nonswag.dev>
2025-08-02 22:31:12 +0200

    Refactor statistics interfaces for shared behavior
    Extracted common functionality into `TypedStat` and updated `BlockTypeStat`, `ItemTypeStat`, and `EntityTypeStat` to extend it. Simplifies code maintenance and improves reusability.

--- api/src/main/java/net/thenextlvl/perworlds/statistics/BlockTypeStat.java
+++ api/src/main/java/net/thenextlvl/perworlds/statistics/BlockTypeStat.java
@@ -4,13 +4,10 @@ import org.bukkit.block.BlockType;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
-import java.util.function.BiConsumer;
-
 /**
  * @since 0.1.0
  */
 @NullMarked
 @ApiStatus.NonExtendable
-public interface BlockTypeStat extends Stat<BlockType> {
-    void forEachValue(BiConsumer<BlockType, Integer> action);
+public interface BlockTypeStat extends TypedStat<BlockType> {
 }
--- api/src/main/java/net/thenextlvl/perworlds/statistics/ItemTypeStat.java
+++ api/src/main/java/net/thenextlvl/perworlds/statistics/ItemTypeStat.java
@@ -4,13 +4,10 @@ import org.bukkit.inventory.ItemType;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
-import java.util.function.BiConsumer;
-
 /**
  * @since 0.1.0
  */
 @NullMarked
 @ApiStatus.NonExtendable
-public interface ItemTypeStat extends Stat<ItemType> {
-    void forEachValue(BiConsumer<ItemType, Integer> action);
+public interface ItemTypeStat extends TypedStat<ItemType> {
 }
