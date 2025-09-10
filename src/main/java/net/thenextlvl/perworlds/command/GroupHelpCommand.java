2059e3f04b3e451bda1ee6887f281ddc8a51f6f8
david <github@nonswag.dev>
2025-09-10 11:42:30 +0200

    Refactor GroupHelpCommand to extend SimpleCommand
    Enhance the GroupHelpCommand by extending SimpleCommand for better command structure and readability. Update the command execution logic to improve message handling.

--- src/main/java/net/thenextlvl/perworlds/command/GroupHelpCommand.java
+++ src/main/java/net/thenextlvl/perworlds/command/GroupHelpCommand.java
@@ -2,17 +2,28 @@ package net.thenextlvl.perworlds.command;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.perworlds.PerWorldsPlugin;
+import net.thenextlvl.perworlds.command.brigadier.SimpleCommand;
 
-final class GroupHelpCommand {
+import static net.thenextlvl.perworlds.PerWorldsPlugin.DOCS_URL;
+
+final class GroupHelpCommand extends SimpleCommand {
+    private GroupHelpCommand(PerWorldsPlugin plugin) {
+        super(plugin, "help", null);
+    }
+    
     public static ArgumentBuilder<CommandSourceStack, ?> create(PerWorldsPlugin plugin) {
-        return Commands.literal("help").executes(context -> {
-            plugin.bundle().sendMessage(context.getSource().getSender(), "perworlds.help",
-                    Placeholder.parsed("documentation", PerWorldsPlugin.DOCS_URL));
-            return Command.SINGLE_SUCCESS;
-        });
+        var command = new GroupHelpCommand(plugin);
+        return command.create().executes(command);
+    }
+
+    @Override
+    public int run(CommandContext<CommandSourceStack> context) {
+        var sender = context.getSource().getSender();
+        plugin.bundle().sendMessage(sender, "perworlds.help", Placeholder.parsed("documentation", DOCS_URL));
+        return Command.SINGLE_SUCCESS;
     }
 }
