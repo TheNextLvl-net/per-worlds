c0b46128ea6151a5412d1e1fd0622ff4d1cefc6c
david <mail@nonswag.dev>
2025-09-26 20:56:28 +0200

    Fix setup success file creation and check

--- src/main/java/net/thenextlvl/perworlds/command/setup/SetupFinishCommand.java
+++ src/main/java/net/thenextlvl/perworlds/command/setup/SetupFinishCommand.java
@@ -6,6 +6,9 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.thenextlvl.perworlds.PerWorldsPlugin;
 import net.thenextlvl.perworlds.command.brigadier.SimpleCommand;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 final class SetupFinishCommand extends SimpleCommand {
     private SetupFinishCommand(PerWorldsPlugin plugin) {
         super(plugin, "finish", "perworlds.command.setup.finish");
@@ -18,6 +21,18 @@ final class SetupFinishCommand extends SimpleCommand {
 
     @Override
     public int run(CommandContext<CommandSourceStack> context) {
-        return 0;
+        var sender = context.getSource().getSender();
+        if (Files.isRegularFile(plugin.setupSuccess)) {
+            plugin.bundle().sendMessage(sender, "setup.finish.already");
+            return 0;
+        } else try {
+            Files.createFile(plugin.setupSuccess);
+            plugin.bundle().sendMessage(sender, "setup.finish.success");
+            return SINGLE_SUCCESS;
+        } catch (IOException e) {
+            plugin.bundle().sendMessage(sender, "setup.finish.failed");
+            plugin.getComponentLogger().error("Failed to create setup success file", e);
+            return 0;
+        }
     }
 }
