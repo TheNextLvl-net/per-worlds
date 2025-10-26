354aa9cec7c4920229e7b813fb3a6ecb505b3930
david <mail@nonswag.dev>
2025-10-26 08:53:41 +0100

    Fixed deserialization error after server update

--- src/main/java/net/thenextlvl/perworlds/adapter/statistic/BlockTypeStatAdapter.java
+++ src/main/java/net/thenextlvl/perworlds/adapter/statistic/BlockTypeStatAdapter.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.key.Key;
 import net.thenextlvl.nbt.serialization.ParserException;
 import net.thenextlvl.nbt.serialization.TagDeserializationContext;
 import net.thenextlvl.nbt.tag.Tag;
+import net.thenextlvl.perworlds.PerWorldsPlugin;
 import net.thenextlvl.perworlds.model.PaperBlockTypeStat;
 import net.thenextlvl.perworlds.statistics.BlockTypeStat;
 import org.bukkit.Registry;
@@ -14,13 +15,20 @@ import java.util.HashMap;
 
 @NullMarked
 public final class BlockTypeStatAdapter extends TypedStatAdapter<BlockTypeStat> {
+    private final PerWorldsPlugin plugin;
+
+    public BlockTypeStatAdapter(PerWorldsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     @SuppressWarnings("PatternValidation")
     public BlockTypeStat deserialize(Tag tag, TagDeserializationContext context) throws ParserException {
         var values = new HashMap<BlockType, Integer>();
         tag.getAsCompound().forEach((type, value) -> {
-            var entity = Registry.BLOCK.getOrThrow(Key.key(type));
-            values.put(entity, value.getAsInt());
+            var entity = Registry.BLOCK.get(Key.key(type));
+            if (entity != null) values.put(entity, value.getAsInt());
+            else plugin.getComponentLogger().warn("Failed to deserialize statistic: Unknown BlockType {}", type);
         });
         return new PaperBlockTypeStat(values);
     }
--- src/main/java/net/thenextlvl/perworlds/adapter/statistic/ItemTypeStatAdapter.java
+++ src/main/java/net/thenextlvl/perworlds/adapter/statistic/ItemTypeStatAdapter.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.key.Key;
 import net.thenextlvl.nbt.serialization.ParserException;
 import net.thenextlvl.nbt.serialization.TagDeserializationContext;
 import net.thenextlvl.nbt.tag.Tag;
+import net.thenextlvl.perworlds.PerWorldsPlugin;
 import net.thenextlvl.perworlds.model.PaperItemTypeStat;
 import net.thenextlvl.perworlds.statistics.ItemTypeStat;
 import org.bukkit.Registry;
@@ -14,13 +15,20 @@ import java.util.HashMap;
 
 @NullMarked
 public final class ItemTypeStatAdapter extends TypedStatAdapter<ItemTypeStat> {
+    private final PerWorldsPlugin plugin;
+
+    public ItemTypeStatAdapter(PerWorldsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     @SuppressWarnings("PatternValidation")
     public ItemTypeStat deserialize(Tag tag, TagDeserializationContext context) throws ParserException {
         var values = new HashMap<ItemType, Integer>();
         tag.getAsCompound().forEach((type, value) -> {
-            var entity = Registry.ITEM.getOrThrow(Key.key(type));
-            values.put(entity, value.getAsInt());
+            var item = Registry.ITEM.get(Key.key(type));
+            if (item != null) values.put(item, value.getAsInt());
+            else plugin.getComponentLogger().warn("Failed to deserialize statistic: Unknown ItemType {}", type);
         });
         return new PaperItemTypeStat(values);
     }
