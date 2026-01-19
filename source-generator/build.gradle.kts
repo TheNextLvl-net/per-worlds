8b9665a2cc34a8e3547a06f34518c80e39a479d0
david <mail@nonswag.dev>
2026-01-19 17:23:44 +0100

    Updated dependencies for 1.21.10
    - Bumped javapoet to 0.10.0
    - Bumped nbt to 4.3.1

--- source-generator/build.gradle.kts
+++ source-generator/build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     implementation("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
-    implementation("com.palantir.javapoet:javapoet:0.9.0")
-    implementation("net.thenextlvl:nbt:4.2.0")
+    implementation("com.palantir.javapoet:javapoet:0.10.0")
+    implementation("net.thenextlvl:nbt:4.3.1")
     implementation(project(":api"))
 
     testImplementation(platform("org.junit:junit-bom:6.1.0-SNAPSHOT"))
