873f7c3987cceecbf16af9d6de4d6771e818dded
david <mail@nonswag.dev>
2026-05-05 21:46:22 +0200

    Remove now redundant public modifier from main

--- source-generator/src/main/java/net/thenextlvl/perworlds/generator/SourceGenerator.java
+++ source-generator/src/main/java/net/thenextlvl/perworlds/generator/SourceGenerator.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 public final class SourceGenerator {
-    public static void main(final String[] args) throws IOException {
+    static void main(final String[] args) throws IOException {
         final var output = Paths.get(args[0]);
         new GroupSettingsAdapterGenerator().writeToFile(output);
     }
